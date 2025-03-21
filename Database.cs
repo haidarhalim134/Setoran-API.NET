0f329d28ab68ff519c6ff42a81e2f57b60d5c453
haidar <haidarah134@gmail.com>
2025-03-21 10:27:38 +0700

    postgresql configuration

--- Database.cs
+++ Database.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using dotenv;
+using dotenv.net;
 
 class Database: IdentityDbContext<User>
 {
@@ -23,4 +25,27 @@ class Database: IdentityDbContext<User>
     => options
         .UseSqlite($"Data Source=./database.db")
         .UseLazyLoadingProxies(false);
+    // {
+    //     kalau udah punya postgres
+    //     DotEnv.Load(); // Load environment variables from .env
+    //     var envVars = DotEnv.Read();
+
+    //     string host = envVars["DB_HOST"];
+    //     string port = envVars["DB_PORT"];
+    //     string dbName = envVars["DB_NAME"];
+    //     string user = envVars["DB_USER"];
+    //     string password = envVars["DB_PASSWORD"];
+
+    //     if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(dbName) || 
+    //         string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
+    //     {
+    //         throw new Exception("Database connection variables are missing!");
+    //     }
+
+    //     string connectionString = $"Host={host};Port={port};Database={dbName};Username={user};Password={password}";
+
+    //     options.UseNpgsql(connectionString);
+    //     options.UseLazyLoadingProxies(false);
+
+    // }
 }
