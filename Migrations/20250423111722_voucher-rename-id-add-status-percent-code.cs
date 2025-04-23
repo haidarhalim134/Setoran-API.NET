aac836fcbe1abe321483f19af6b0b648be5ffd2b
haidar <haidarah134@gmail.com>
2025-04-23 18:18:48 +0700

    voucher rename id + add status, persen, nama voucher

---
+++ Migrations/20250423111722_voucher-rename-id-add-status-percent-code.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Setoran_API.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class voucherrenameidaddstatuspercentcode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Potongan",
+                table: "Voucher");
+
+            migrationBuilder.RenameColumn(
+                name: "Tipe",
+                table: "Voucher",
+                newName: "NamaVoucher");
+
+            migrationBuilder.RenameColumn(
+                name: "Kode",
+                table: "Voucher",
+                newName: "KodeVoucher");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Voucher",
+                newName: "IdVoucher");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PersenVoucher",
+                table: "Voucher",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusVoucher",
+                table: "Voucher",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersenVoucher",
+                table: "Voucher");
+
+            migrationBuilder.DropColumn(
+                name: "StatusVoucher",
+                table: "Voucher");
+
+            migrationBuilder.RenameColumn(
+                name: "NamaVoucher",
+                table: "Voucher",
+                newName: "Tipe");
+
+            migrationBuilder.RenameColumn(
+                name: "KodeVoucher",
+                table: "Voucher",
+                newName: "Kode");
+
+            migrationBuilder.RenameColumn(
+                name: "IdVoucher",
+                table: "Voucher",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Potongan",
+                table: "Voucher",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
--- Models/Voucher.cs
+++ Models/Voucher.cs
@@ -5,11 +5,28 @@ namespace Setoran_API.NET.Models
     public class Voucher
     {
         [Key]
-        public int Id { get; set; }
-        public string Kode { get; set; }
-        public decimal Potongan { get; set; }
-        public string Tipe { get; set; }
+        public int IdVoucher { get; set; }
+        public string NamaVoucher { get; set; }
+        public StatusVoucher StatusVoucher { get; set; }
         public DateTime TanggalMulai { get; set; }
         public DateTime TanggalAkhir { get; set; }
+        
+        [Range(1, 100)]
+        public int PersenVoucher { get; set; }
+        public string KodeVoucher { get; set; }
+
+        public List<Voucher> GetActive(Database db)
+        {
+            return db.Voucher
+                .Where(v => v.StatusVoucher == StatusVoucher.Aktif)
+                .Where(v => v.TanggalMulai <= DateTime.Now && DateTime.Now <= v.TanggalAkhir)
+                .ToList();
+        }
+        
+    }
+    public enum StatusVoucher
+    {
+        Aktif,
+        NonAktif
     }
 }
