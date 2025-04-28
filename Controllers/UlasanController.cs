22de4152409ad022e061ef79caf96bb5bdfc0402
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2025-04-29 00:10:27 +0700

    Add UlasanController and PostUlasanDTO

---
+++ Controllers/UlasanController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Setoran_API.NET.Models;
+
+namespace Setoran_API.NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UlasanController : ControllerBase
+    {
+        Database _context;
+
+        public UlasanController(Database context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUlasan()
+        {
+            var ulasan = await _context.Ulasan.ToListAsync();
+            return Ok(new
+            {
+                success = true,
+                data = ulasan,
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUlasanById(int id)
+        {
+            var ulasan = await _context.Ulasan.FindAsync(id);
+            if (ulasan == null)
+            {
+                return NotFound(new { success = false, message = "Ulasan tidak ditemukan" });
+            }
+            return Ok(new
+            {
+                success = true,
+                data = ulasan,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUlasan([FromBody] PostUlasanDTO ulasan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newUlasan = new Ulasan
+            {
+                IdMotor = ulasan.IdMotor,
+                IdPelanggan = ulasan.IdPelanggan,
+                Rating = ulasan.Rating,
+                Komentar = ulasan.Komentar,
+                TanggalUlasan = DateTime.Now
+            };
+
+            _context.Ulasan.Add(newUlasan);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUlasanById), new { id = newUlasan.IdUlasan }, newUlasan);
+        }
+    }
+}
