070e23db79ab6cc735f7ad9e91c4d2782d0deaa3
haidar <haidarah134@gmail.com>
2025-05-03 18:37:52 +0700

    Create pengguna dto and move register form

---
+++ Models/Pengguna/PostPenggunaDTO.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using Mapster;
+
+namespace Setoran_API.NET.Models;
+
+public class PostPenggunaDTO
+{
+    [Required]
+    public string Id { get; set; }
+    public string Nama { get; set; }
+    public DateTime? TanggalLahir { get; set; }
+    public string NomorTelepon { get; set; }
+    public int? Umur { get; set; }
+    public string NomorKTP { get; set; }
+    public string Alamat { get; set; }
+    public string? IdGambar { get; set; }
+
+    public Pengguna ToPengguna()
+    {
+        return this.Adapt<Pengguna>();
+    }
+}
