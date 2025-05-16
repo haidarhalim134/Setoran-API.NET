6761bf840e132c1fc4ff167a670cb7f36e9ba853
haidar <haidarah134@gmail.com>
2025-05-16 09:58:53 +0700

    add isadmin to pengguna

---
+++ Migrations/20250516022242_update pengguna, add isadmin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Setoran_API.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatepenggunaaddisadmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+        }
+    }
+}
