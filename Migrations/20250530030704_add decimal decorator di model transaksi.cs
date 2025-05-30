6c559d0fb29783a32309c9d2bbe33cc00991eba2
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2025-05-30 10:08:23 +0700

    update TotalHarga column type to decimal(18,2) in Transaksi model and migration

---
+++ Migrations/20250530030704_add decimal decorator di model transaksi.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Setoran_API.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddecimaldecoratordimodeltransaksi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalHarga",
+                table: "Transaksi",
+                type: "numeric(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalHarga",
+                table: "Transaksi",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+        }
+    }
+}
