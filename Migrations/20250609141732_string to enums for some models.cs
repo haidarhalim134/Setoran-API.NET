3d5a1f96910b839883da7b199e93f4a52869485e
haidar <haidarah134@gmail.com>
2025-06-09 21:23:10 +0700

    create migrations

---
+++ Migrations/20250609141732_string to enums for some models.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Setoran_API.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class stringtoenumsforsomemodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Transaksi.Status
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Transaksi");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Transaksi",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            // Pembayaran.StatusPembayaran
+            migrationBuilder.DropColumn(
+                name: "StatusPembayaran",
+                table: "Pembayaran");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusPembayaran",
+                table: "Pembayaran",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            // Pembayaran.MetodePembayaran
+            migrationBuilder.DropColumn(
+                name: "MetodePembayaran",
+                table: "Pembayaran");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MetodePembayaran",
+                table: "Pembayaran",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            // Motor.Transmisi
+            migrationBuilder.DropColumn(
+                name: "Transmisi",
+                table: "Motor");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Transmisi",
+                table: "Motor",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            // Motor.StatusMotor
+            migrationBuilder.DropColumn(
+                name: "StatusMotor",
+                table: "Motor");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusMotor",
+                table: "Motor",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Transaksi",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusPembayaran",
+                table: "Pembayaran",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MetodePembayaran",
+                table: "Pembayaran",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Transmisi",
+                table: "Motor",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusMotor",
+                table: "Motor",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
